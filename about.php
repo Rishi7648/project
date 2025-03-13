28fe5ab250d1ec9fbbd522f268240164cee4c8c1
rishi <rishisilwal19@gmail.com>
2025-03-13 11:23:56 +0545

    commit message

---
+++ about.php
@@ -0,0 +1,98 @@
+<?php  
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About Us</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<!-- about section starts  -->
+
+<section class="about">
+
+   <div class="row">
+      <div class="image">
+         <img src="images/about-img.svg" alt="">
+      </div>
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorem provident voluptatum distinctio laborum veritatis vitae suscipit praesentium fugiat unde?</p>
+        
+      </div>
+   </div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- steps section starts  -->
+
+   <div class="box-container">
+
+      
+
+   
+</section>
+
+<!-- steps section ends -->
+
+<!-- review section starts  -->
+
+
+   <div class="box-container">
+
+      
+
+      <
+
+      <
+
+      
+
+      
+
+   </div>
+
+</section>
+
+<!-- review section ends -->
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,29 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+        
+         <a href="mailto:rishisilwal19@gmail.com"><i class="fas fa-envelope"></i><span>rishisilwal19@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>Location:Thamel kathmandu</span></a>
+         <a href="#"><i class=""></i><span>Telephone No: 77363788</span></a>
+         
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Ghar Jagga Sansar</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
